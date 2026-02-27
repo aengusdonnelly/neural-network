7dc772041d93f93b5bd6a86de081f71e73b0e61a
aengus <aengus@kth.se>
2026-02-27 18:20:34 +0100

    Added initialization and prediction method, as well as a small test in the application file

--- MNist_Application.py
+++ MNist_Application.py
@@ -1,34 +1,23 @@
 import numpy as np
 import random as rm
 import matplotlib.pyplot as plt
+import NeuralNetwork as nn
 
 class MNist():
     
     def __init__(self):
-
         self.ims_train = np.load('images_train.npy')
         self.lbs_train = np.load('labels_train.npy')
         self.ims_test = np.load('images_test.npy')
         self.lbs_test = np.load('labels_test.npy')
 
 def main():
-    mn = MNist()
 
-    test = True
+    def sigmoid(x):
+        return 1 / (1 - np.exp(-x))
 
-    if test:
-    
-        i = rm.randint(0, len(mn.ims_train))
-        j = rm.randint(0, len(mn.ims_test))
-
-        plt.imshow(mn.ims_train[i], cmap='gray')
-        plt.title("Number: "+str((mn.lbs_train[i])))
-        plt.show()
-
-        plt.imshow(mn.ims_test[j], cmap='gray')
-        plt.title("Number: "+str((mn.lbs_test[j])))
-        plt.show()
-
+    NN = nn.Network(layers=(10, 5, 5, 3), activation=sigmoid)
+    NN.predict(np.random.rand(10, 1))
 
 if __name__ == "__main__":
     main()
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,10 +1,27 @@
 import numpy as np
 
-class NeuralNetwork():
+class Network():
 
-    def __init__(self):
-        self.layers = None
-        self.net = None
+    def __init__(self, layers, activation):
 
-    def initialize(layers):
+        self.ls = layers
+        self.Ws = []
+        self.bs = []
+        self.activation = activation
+
+        for i in range(1, len(self.ls)):
+            W = np.random.rand(self.ls[i], self.ls[i-1])
+            b = np.random.rand(self.ls[i], 1)
+            self.Ws.append(W)
+            self.bs.append(b)
+
+    def train(self):
+        #TODO: Write a method for back-propagation.
+        pass
+
+    def predict(self, a):
         
+        for i in range(len(self.Ws)):
+            a = self.activation(np.dot(self.Ws[i], a) - self.bs[i])
+
+        return a
