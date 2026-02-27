1d8836a11d444ae57b0ba770b5dbf2b779a5dc92
aengus <aengus@kth.se>
2026-02-27 15:25:43 +0100

    First small edits

---
+++ MNist_Application.py
@@ -0,0 +1,13 @@
+import numpy as np
+import NeuralNetwork as nn
+
+class MNist():
+    
+    def __inti__(self):
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ NeuralNetwork.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class NeuralNetwork():
+
+    def __init__(self, load=True, filename="MyNeuralNetwork"):
+
+        if load == True:
+            pass
+
+        else:
+            self.label = filename
+            self.layers = None
+            self.net = None
+
+    def initialize(layers):
+        pass
